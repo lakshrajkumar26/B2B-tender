23e168b5b384dc119536c42ae82a36a8386c23ca
lakshrajkumar26 <110077095+lakshrajkumar26@users.noreply.github.com>
2025-07-05 17:24:56 +0530

    Update Protected.jsx

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import API_BASE_URL from '../configFolder/api.js';
+import API_BASE_URL from '../configFolder/api';
 
 const ProtectedPage = () => {
   const [message, setMessage] = useState('');

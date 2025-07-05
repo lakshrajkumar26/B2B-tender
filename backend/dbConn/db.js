e9b35d232eff3470720b8aee5bf7d49e90d0c33b
lakshrajkumar26 <110077095+lakshrajkumar26@users.noreply.github.com>
2025-07-05 10:19:06 +0530

    Update db.js

--- backend/dbConn/db.js
+++ backend/dbConn/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require('dotenv').config();
 
 // const URL = "mongodb://localhost:27017/newuser" 
-const URL = process.env.MONGO_URI || process.env.MONGO_URI
+const URL = process.env.MONGODB_URL || process.env.MONGO_URL 
 
 if (!URL) {
     console.error("MongoDB URI is not defined. Please set MONGODB_URI environment variable.");

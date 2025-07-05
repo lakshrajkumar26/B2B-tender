0ec4ffe4a5e36e00e3e267895357d501b762801b
lakshrajkumar26 <110077095+lakshrajkumar26@users.noreply.github.com>
2025-07-05 10:38:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -69,8 +69,8 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running' });
 });
 
-const PORT = process.env.PORT || 3000;
 
+const PORT = parseInt(process.env.PORT) || 3000;
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
